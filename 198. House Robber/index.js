558a87f9e21a6d76da4391a9db4a1573feb9db78
ToufiqAlahi <nipuntan01@gmail.com>
2021-12-22 23:07:22 +0600

    Update index.js

--- 198. House Robber/index.js
+++ 198. House Robber/index.js
@@ -6,7 +6,7 @@ var rob = function (nums) {
 
     if (nums.length === 0) return 0;
     if (nums.length === 1) return nums[0];
-    if (nums.length === 0) return Math.max(nums[0], nums[1]);
+    if (nums.length === 2) return Math.max(nums[0], nums[1]);
 
     let maxRobAtNth = [nums[0], Math.max(nums[0], nums[1])];
     for (let i = 2; i < nums.length; i++) {
