773f9b8a7af747d33288614bc8c85eb778562443
ToufiqAlahi <nipuntan01@gmail.com>
2021-12-22 22:26:46 +0600

    Update index.js

--- 70. Climbing Stairs/index.js
+++ 70. Climbing Stairs/index.js
@@ -1,3 +1,16 @@
-const climbStairs = n => {};
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function (n) {
+    if (n <= 3) {
+        return n;
+    }
+    let ways = [1, 2, 3, 5];
+    for (let i = 0; i <= n; i++) {
+        ways.push(ways[i - 1] + ways[i - 2]);
+    }
+    return ways[n];
+};
 
 module.exports = climbStairs;
