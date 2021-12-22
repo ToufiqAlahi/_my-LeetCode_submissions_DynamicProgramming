5feb27c5915ac02dc39b2b5c18bf28153ef6a4e8
ToufiqAlahi <nipuntan01@gmail.com>
2021-12-23 01:08:09 +0600

    Update index.js

--- 55. Jump Game/index.js
+++ 55. Jump Game/index.js
@@ -1,3 +1,26 @@
-function canJump(nums) {}
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+
+
+var canJump = function (nums) {
+
+    let lastValidIndex = nums.length - 1;
+
+    for (let i = nums.length - 1; i >= 0; i--) {
+
+        if (i + nums[i] >= lastValidIndex) {
+            lastValidIndex = i;
+
+        }
+    }
+
+    return lastValidIndex === 0;
+};
+
+// [2,3,1,1,4] shows true
+
+//[3,2,1,0,4] shows false
 
 module.exports = canJump;
