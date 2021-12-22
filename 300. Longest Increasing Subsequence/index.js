20b8d06b9f06f6cfbd1cc12f1283431b02925357
ToufiqAlahi <nipuntan01@gmail.com>
2021-12-23 05:24:42 +0600

    Update index.js

--- 300. Longest Increasing Subsequence/index.js
+++ 300. Longest Increasing Subsequence/index.js
@@ -3,7 +3,23 @@
  * @return {number}
  */
 var lengthOfLIS = function (nums) {
+    if (nums.length === 0) return 0;
 
+    let subSequence = new Array(nums.length).fill(1);
+    let maxSoFar = 1;
+    for (let j = 1; j < nums.length; j++) {
+
+        for (let i = 0; i < j; i++) {
+            if (nums[j] > nums[i]) {
+                subSequence[j] = Math.max(subSequence[i] + 1, subSequence[j]);
+            }
+
+        }
+        maxSoFar = Math.max(maxSoFar, subSequence[j]);
+
+    }
+
+    return maxSoFar;
 };
 
 module.exports = lengthOfLIS;
