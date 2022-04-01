0c15df47662f81bdac29ce67155b8fe57a2550b2
ToufiqAlahi <nipuntan01@gmail.com>
2022-04-01 09:21:30 +0600

    Update test.js

--- 7. Reverse Integer/test.js
+++ 7. Reverse Integer/test.js
@@ -1,13 +1,13 @@
 const jump = require("./index");
 
 test("[2, 3, 1, 1, 4] returns true", () => {
-  expect(jump([2, 3, 1, 1, 4])).toEqual(true);
+    expect(jump([2, 3, 1, 1, 4])).toEqual(true);
 });
 
 test("[3] returns true", () => {
-  expect(jump([3])).toEqual(true);
+    expect(jump([3])).toEqual(true);
 });
 
 test("[3,2,1,0,4] returns false", () => {
-  expect(jump([3, 2, 1, 0, 4])).toEqual(false);
+    expect(jump([3, 2, 1, 0, 4])).toEqual(false);
 });
