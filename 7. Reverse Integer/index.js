972c6bd4fe30fff5e5c29a3c4d1c4ded92049d72
ToufiqAlahi <nipuntan01@gmail.com>
2022-04-01 09:43:57 +0600

    Update index.js

--- 7. Reverse Integer/index.js
+++ 7. Reverse Integer/index.js
@@ -3,8 +3,17 @@
  * @return {number}
  */
 var reverse = function (x) {
+    let negative = x < 0;
+    let reversed = 0;
+    if (negative) { x = x * -1; }
+    while (x > 0) {
+        reversed = reversed * 10 + x % 10;
+        x = Math.floor(x / 10);
+    }
+    if (reversed > 2 ** 31 - 1) {
+        return 0;
+    }
+    return negative ? reversed * -1 : reversed;
 
 };
-
-
-module.exports = jump;
+module.exports = reverse;
